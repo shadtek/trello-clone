86bb2f035b17de3a76e4b891ad780483a9bbf966
shadtek <spencer.shattuck@gmail.com>
2016-07-02 09:51:26 -0600

    simple server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var app = express();
 
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Trello clone app listening on port 3000!');
+});
