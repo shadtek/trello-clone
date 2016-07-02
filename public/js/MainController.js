544534824118c06f34ae809f9d59d6303e7142a7
shadtek <spencer.shattuck@gmail.com>
2016-07-02 10:20:09 -0600

    get worked

--- public/js/MainController.js
+++ public/js/MainController.js
@@ -1,5 +1,15 @@
 angular.module('Trello')
 
 .controller('MainController', function ($scope, $http) {
-  
+	
+	// $http.get('/hello-world')
+	// .then(function(response){
+	// 	console.log(response.data);
+	// });
+
+	var newTask = {
+		text: 'Do laundry.'
+	}
+
+
 });
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
 app.use(express.static('public'));
+
+app.get('/hello-world', function (req, res) {
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
   console.log('Trello clone app listening on port 3000!');
